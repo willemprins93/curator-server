4bb861007719d50e2d2bd41f0383d92d48d9bcfe
Willem Prins <willemprins93@gmail.com>
2020-10-29 19:21:30 +0000

    update app.js

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => res.json({ true }));
 app.use("/auth", authRouter);
 app.use("/artwork", artworkRouter);
 
